b7db1cde10003f17ba2bff245afe9f66ea05962a
Ayumi <ayuumiikoo@gmail.com>
2026-03-08 16:45:15 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'default-dev-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL', 
-        'postgresql://postgres:password@localhost:5432/tastelab'
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
